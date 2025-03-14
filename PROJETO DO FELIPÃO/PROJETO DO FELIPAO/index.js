b43f62041269c774b0085b34c59cecb17c7a61d8
Luiza-Moonieve <154467515+Moonieve@users.noreply.github.com>
2025-03-13 20:11:58 -0400

    index.js
    - A JavaScript project
    - Projecet demanded by: Felipão
    - My first repository coding
    - Level of the hero
    
    03/13

--- PROJETO DO FELIPÃO/PROJETO DO FELIPAO/index.js
+++ PROJETO DO FELIPÃO/PROJETO DO FELIPAO/index.js
@@ -1,31 +1,65 @@
-//jornada do hero
+//project 01.
 let hero = "Patroclus";
 
 let XP = 8000;  
-let nivel = " ";
+let level = " ";
 
-if (XP < 1000) {
-  nivel = "iron";
+if (XP <= 1000) {
+  level = "iron";
 } 
 else if (XP >= 1001 && XP <= 2000) {
-  nivel = "bronze";
+  level = "bronze";
 } 
 else if (XP >= 2001 && XP <= 5000) {
-  nivel = "silver";
+  level = "silver";
 }
 else if (XP >= 7001 && XP <= 8000) {
-  nivel = "platinum";
+  level = "platinum";
 }
 else if (XP >= 8001 && XP <= 9000) {
-  nivel = "arising";
+  level = "arising";
 }
 else if (XP >= 9001 && XP <= 10000) {
-  nivel = "immortal";
+  level = "immortal";
 }
 else if (XP >= 10001) {
-  nivel = "radiant";
+  level = "radiant";
 }
 
-console.log("The hero of name " + hero + " is on the level " + nivel);
+console.log("The hero of name " + hero + " is on the level " + level);
+
+
+
+
+
+//project 02. I got obsessed about to do it in a different way.
+
+let mana = 10000;  
+let King = "Patroclus";
+
+let levels = [
+  { minMana: 0, maxMana: 1000, level: "Iron" },
+  { minMana: 1001, maxMana: 2000, level: "Bronze" },
+  { minMana: 2001, maxMana: 5000, level: "Silver" },
+  { minMana: 5001, maxMana: 7000, level: "Gold" },
+  { minMana: 7001, maxMana: 8000, level: "Platinium" },
+  { minMana: 8001, maxMana: 9000, level: "Arising" },
+  { minMana: 9001, maxMana: 10000, level: "Immortal" },
+  { minMana: 10001, maxMana: Infinity, level: "Radiant" }
+];
+
+function getKingLevel(mana) {
+  for (let i = 0; i < levels.length; i++) {
+    if (mana >= levels[i].minMana && mana <= levels[i].maxMana) {
+      return levels[i].level;
+    }
+  }
+  return "Unknown";  
+}
+
+let kingLevel = getKingLevel(mana);
+
+console.log(`The hero ${King} is on the level ${kingLevel}`);
+
 
 
